060bdb4b9adab71cff7c241d74151a743abf8d69
Anmol100399 <anmolverma0101@email.com>
2024-11-29 17:08:11 -0500

    adding staff MVC and views

--- ArtOnWheels/ArtOnWheels/Controllers/StaffPageController.cs
+++ ArtOnWheels/ArtOnWheels/Controllers/StaffPageController.cs
@@ -1,12 +1,89 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ArtOnWheels.Interfaces;
+using ArtOnWheels.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace ArtOnWheels.Controllers
 {
     public class StaffPageController : Controller
     {
-        public IActionResult Index()
+        private readonly IStaffService _staffService;
+
+        public StaffPageController(IStaffService staffService)
+        {
+            _staffService = staffService;
+        }
+
+        public async Task<IActionResult> List()
+        {
+            var staffs = await _staffService.ListStaffs();
+            return View(staffs);
+        }
+
+        public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(StaffDto staffDto)
+        {
+            ServiceResponse response = await _staffService.CreateStaff(staffDto);
+
+            if (response.Status == ServiceResponse.ServiceStatus.Created)
+            {
+                return RedirectToAction("List", "StaffPage");
+            }
+            else
+            {
+                return View("Error", new ErrorViewModel() { Errors = response.Messages });
+            }
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var staff = await _staffService.GetStaff(id);
+            if (staff == null)
+                return NotFound();
+
+            return View(staff);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, StaffDto staffDto)
+        {
+            ServiceResponse response = await _staffService.UpdateStaff(id, staffDto);
+
+            if (response.Status == ServiceResponse.ServiceStatus.Updated)
+            {
+                return RedirectToAction("List", "StaffPage");
+            }
+            else
+            {
+                return View("something went wrong");
+            }
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var staff = await _staffService.GetStaff(id);
+            if (staff == null)
+                return NotFound();
+
+            return View(staff);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            ServiceResponse response = await _staffService.DeleteStaff(id);
+            if (response.Status == ServiceResponse.ServiceStatus.Deleted)
+            {
+                return RedirectToAction("List", "StaffPage");
+            }
+            else
+            {
+                return View("Error", new ErrorViewModel() { Errors = response.Messages });
+            };
         }
     }
 }
---
+++ ArtOnWheels/ArtOnWheels/Data/Migrations/20241129213844_create-migration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ArtOnWheels.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class createmigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
