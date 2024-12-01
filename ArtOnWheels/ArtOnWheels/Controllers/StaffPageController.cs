02330c3694fefd8920e419bc99ac54f87f436d8d
Drashti-29 <pokiyadrashti29@gmail.com>
2024-12-01 01:06:02 -0500

    Updated autority for staff details access

--- ArtOnWheels/ArtOnWheels/Controllers/StaffPageController.cs
+++ ArtOnWheels/ArtOnWheels/Controllers/StaffPageController.cs
@@ -3,108 +3,105 @@ using ArtOnWheels.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
-public class StaffPageController : Controller
+namespace ArtOnWheels.Controllers
 {
-    private readonly IStaffService _staffService;
-
-    public StaffPageController(IStaffService staffService)
-    {
-        _staffService = staffService;
-    }
-    public IActionResult Index()
-    {
-        return RedirectToAction("List");
-    }
-
-    public async Task<IActionResult> List()
-    {
-        var staffs = await _staffService.ListStaffs();
-        return View(staffs);
-    }
-
-    [HttpGet]
-    public async Task<IActionResult> Details(int id)
-    {
-        var staff = await _staffService.GetStaff(id);
-        if (staff == null)
-        {
-            return NotFound();
-        }
-        return View(staff);
-    }
-
     [Authorize(Roles = "Admin")]
-    public IActionResult Create()
+    public class StaffPageController : Controller
     {
-        return View();
-    }
+        private readonly IStaffService _staffService;
 
-    [HttpPost]
-    [Authorize(Roles = "Admin")]
-    public async Task<IActionResult> Create(StaffDto staffDto)
-    {
-        ServiceResponse response = await _staffService.CreateStaff(staffDto);
-
-        if (response.Status == ServiceResponse.ServiceStatus.Created)
+        public StaffPageController(IStaffService staffService)
         {
-            return RedirectToAction("List", "StaffPage");
+            _staffService = staffService;
         }
-        else
+        public IActionResult Index()
         {
-            return View("Error", new ErrorViewModel() { Errors = response.Messages });
+            return RedirectToAction("List");
         }
-    }
 
-    [Authorize(Roles = "Admin")]
-    public async Task<IActionResult> Edit(int id)
-    {
-        var staff = await _staffService.GetStaff(id);
-        if (staff == null)
-            return NotFound();
-
-        return View(staff);
-    }
-
-    [HttpPost]
-    [Authorize(Roles = "Admin")]
-    public async Task<IActionResult> Edit(int id, StaffDto staffDto)
-    {
-        ServiceResponse response = await _staffService.UpdateStaff(id, staffDto);
-
-        if (response.Status == ServiceResponse.ServiceStatus.Updated)
+        public async Task<IActionResult> List()
         {
-            return RedirectToAction("List", "StaffPage");
+            var staffs = await _staffService.ListStaffs();
+            return View(staffs);
         }
-        else
+
+        public IActionResult Create()
         {
-            return View("something went wrong");
+            return View();
         }
-    }
 
-    [Authorize(Roles = "Admin")]
-
-    public async Task<IActionResult> Delete(int id)
-    {
-        var staff = await _staffService.GetStaff(id);
-        if (staff == null)
-            return NotFound();
-
-        return View(staff);
-    }
-
-    [HttpPost, ActionName("Delete")]
-    [Authorize(Roles = "Admin")]
-    public async Task<IActionResult> DeleteConfirmed(int id)
-    {
-        ServiceResponse response = await _staffService.DeleteStaff(id);
-        if (response.Status == ServiceResponse.ServiceStatus.Deleted)
+        [HttpGet]
+        public async Task<IActionResult> Details(int id)
         {
-            return RedirectToAction("List", "StaffPage");
+            var staff = await _staffService.GetStaff(id);
+            if (staff == null)
+            {
+                return NotFound();
+            }
+            return View(staff);
         }
-        else
-        {
-            return View("Error", new ErrorViewModel() { Errors = response.Messages });
-        };
-    }
 
+        [HttpPost]
+        public async Task<IActionResult> Create(StaffDto staffDto)
+        {
+            ServiceResponse response = await _staffService.CreateStaff(staffDto);
+
+            if (response.Status == ServiceResponse.ServiceStatus.Created)
+            {
+                return RedirectToAction("List", "StaffPage");
+            }
+            else
+            {
+                return View("Error", new ErrorViewModel() { Errors = response.Messages });
+            }
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var staff = await _staffService.GetStaff(id);
+            if (staff == null)
+                return NotFound();
+
+            return View(staff);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, StaffDto staffDto)
+        {
+            ServiceResponse response = await _staffService.UpdateStaff(id, staffDto);
+
+            if (response.Status == ServiceResponse.ServiceStatus.Updated)
+            {
+                return RedirectToAction("List", "StaffPage");
+            }
+            else
+            {
+                return View("something went wrong");
+            }
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var staff = await _staffService.GetStaff(id);
+            if (staff == null)
+                return NotFound();
+
+            return View(staff);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            ServiceResponse response = await _staffService.DeleteStaff(id);
+            if (response.Status == ServiceResponse.ServiceStatus.Deleted)
+            {
+                return RedirectToAction("List", "StaffPage");
+            }
+            else
+            {
+                return View("Error", new ErrorViewModel() { Errors = response.Messages });
+            };
+        }
+
+    }
 }
